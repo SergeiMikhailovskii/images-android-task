653ba4cfcf4d1566e23652e2709fd759bad4bf81
sergei-mikhailovskii-idf <sergei.mikhailovskii@idfinance.com>
2023-05-28 22:48:00 +0300

    Added build config

--- data/src/main/java/com/mikhailovskii/data/service/NetworkService.kt
+++ data/src/main/java/com/mikhailovskii/data/service/NetworkService.kt
@@ -1,19 +1,20 @@
 package com.mikhailovskii.data.service
 
+import com.mikhailovskii.data.BuildConfig
 import com.mikhailovskii.data.model.private_area.ImagesListResponse
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
-interface NetworkService {
-    @GET("api/?key=1631539-db8210cabd2636c6df59812df")
+fun interface NetworkService {
+    @GET("api/?key=${BuildConfig.API_KEY}")
     fun getImages(): Call<ImagesListResponse>
 }
 
 fun buildService(): NetworkService {
     val retrofit = Retrofit.Builder()
-        .baseUrl("https://pixabay.com/")
+        .baseUrl(BuildConfig.BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
