324af5b80653d9155e0ec89342a4a8769500511f
sergei-mikhailovskii-idf <sergei.mikhailovskii@idfinance.com>
2023-05-28 22:18:15 +0300

    Added toolbar to private area

--- app/src/main/java/com/mikhailovskii/images_android_task/component/PrivateAreaActivity.kt
+++ app/src/main/java/com/mikhailovskii/images_android_task/component/PrivateAreaActivity.kt
@@ -2,11 +2,32 @@ package com.mikhailovskii.images_android_task.component
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.mikhailovskii.images_android_task.R
 
 class PrivateAreaActivity : AppCompatActivity() {
+
+    private var navController: NavController? = null
+
+    private val appBarConfiguration by lazy { AppBarConfiguration(topLevelDestinationIds = setOf(R.id.homeFragment)) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_private_area)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.fcv_private_area,
+        ) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        setupActionBarWithNavController(navHostFragment.navController, appBarConfiguration)
     }
+
+    override fun onSupportNavigateUp() = navController?.navigateUp(appBarConfiguration) ?: false
 }
