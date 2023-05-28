a984a4b935d2e57eec5f9ec468d4f2ab7be67dd6
sergei-mikhailovskii-idf <sergei.mikhailovskii@idfinance.com>
2023-05-28 20:24:33 +0300

    Added ui to details fragment

--- app/src/main/java/com/mikhailovskii/images_android_task/ui/private_area/details/DetailsFragment.kt
+++ app/src/main/java/com/mikhailovskii/images_android_task/ui/private_area/details/DetailsFragment.kt
@@ -1,7 +1,10 @@
 package com.mikhailovskii.images_android_task.ui.private_area.details
 
 import android.os.Bundle
+import android.view.View
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.mikhailovskii.images_android_task.R
 import com.mikhailovskii.images_android_task.base.BaseFragment
 import com.mikhailovskii.images_android_task.base.ViewBindingStrategy
 import com.mikhailovskii.images_android_task.databinding.FragmentDetailsBinding
@@ -12,9 +15,22 @@ class DetailsFragment : BaseFragment(), ViewBindingStrategy<FragmentDetailsBindi
 
     private val args by navArgs<DetailsFragmentArgs>()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        println(args.payload)
-    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val payload = args.payload ?: return
 
+        binding.tvSize.text = getString(R.string.image_details_size, payload.size)
+        binding.tvType.text = getString(R.string.image_details_type, payload.type)
+        binding.tvTags.text = getString(R.string.image_details_tags, payload.tags)
+        binding.tvUserName.text = getString(R.string.image_details_user_name, payload.userName)
+        binding.tvViews.text = getString(R.string.image_details_views, payload.views)
+        binding.tvLikes.text = getString(R.string.image_details_likes, payload.likes)
+        binding.tvComments.text = getString(R.string.image_details_comments, payload.comments)
+        binding.tvFavorites.text = getString(R.string.image_details_favorites, payload.favorites)
+        binding.tvDownloads.text = getString(R.string.image_details_downloads, payload.downloads)
+
+        Glide.with(requireContext())
+            .load(payload.url)
+            .into(binding.ivImage)
+    }
 }
